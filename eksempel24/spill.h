7722f5cda3c3549d8c6c635e1303fd231975aa6a
halftan <halftan@saetherskar.no>
2015-11-16 15:51:39 +0100

    nop

---
+++ eksempel24/spill.h
@@ -0,0 +1,18 @@
+﻿/*
+    C -programmering, en innføring. 
+
+    Eksempel 24
+
+*/
+
+typedef struct spill_st {
+    int poeng;
+    int status;
+} Spill;
+
+Spill * Spill_opprett();
+int Spill_start( Spill * );
+int Spill_pause( Spill * );
+int Spill_stopp( Spill * );
+int Spill_slett( Spill ** );
+
