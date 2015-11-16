4ffd74ee42dd5db5990a64782b8b05e87e7265ef
halftan <halftan@saetherskar.no>
2015-11-16 12:17:05 +0100

    nop

---
+++ eksempel22/test.c
@@ -0,0 +1,30 @@
+﻿/*
+    C -programmering, en innføring. 
+
+    Eksempel 22
+
+    Hensikten med eksempelet er å vise hvordan man kan bruke en struc
+    for å lage en kompleks variabel.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    struct punkt {
+        int x;
+        int y;
+    };
+
+    struct punkt p;
+
+    p.x = 2;
+    p.y = 3;
+    
+    printf("Mitt punkt p(%d,%d)\n",p.x, p.y);
+    
+    return 0;
+
+}
+
---
+++ eksempel23/test.c
@@ -0,0 +1,33 @@
+﻿/*
+    C -programmering, en innføring. 
+
+    Eksempel 23
+
+    Hensikten med eksempelet er å vise hvordan man kan bruke en struc for å
+    lage en kompleks variabel. Videre viser eksempelet hvordan man ved
+    hjelp av kommandoen typedef, kan opprette en ny type variabel.
+
+*/
+
+#include <stdio.h>
+
+struct punkt {
+    int x;
+    int y;
+};
+
+typedef struct punkt Punkt;
+
+int main() {
+
+    Punkt p;
+
+    p.x = 2;
+    p.y = 3;
+    
+    printf("Mitt punkt p(%d,%d)\n",p.x, p.y);
+    
+    return 0;
+
+}
+
