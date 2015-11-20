c495b990ca82f0bc864a2746da2ba44d99bd2326
halftan <halftan@saetherskar.no>
2015-11-20 12:21:54 +0100

    nop

---
+++ eksempel21/main.c
@@ -0,0 +1,26 @@
+﻿/*
+    C -programmering, en innføring. 
+
+    Eksempel 21
+
+    Hensikten med eksempelet er å vise hvordan man kan 
+    lage et objektorientert design i C
+
+*/
+
+#include <stdio.h>
+#include <spill.h>
+
+int main() {
+
+    Spill *spill = Spill_opprett();
+
+    Spill_start(spill);
+    Spill_pause(spill);
+    Spill_stop(spill);
+
+    Spill_slett(&spill);
+    return 0;
+
+}
+
